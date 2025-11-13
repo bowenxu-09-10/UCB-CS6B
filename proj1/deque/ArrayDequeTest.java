ae33e5c219cb1e3d605928006c10e2189cbf7b0a
Bowen <bowenxu.09.10@gmail.com>
2025-11-13 17:07:44 +0800

    Add remove and get Test

--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -3,14 +3,46 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ArrayDequeTest {
-    /** Test if the addFirst and isEmpty funciton works well*/
+    /** Test if the add and isEmpty funciton works well*/
     @Test
-    public void addFirstIsEmptyTest() {
+    public void addIsEmptyTest() {
         ArrayDeque<String> alst = new ArrayDeque();
         assertEquals(true, alst.isEmpty());
-        alst.addFirst("I");
-        alst.addFirst("Love");
-        alst.addFirst("Toby");
+        for (int i = 0; i < 10; i++) {
+            alst.addFirst("Me");
+            alst.addLast("Love");
+            alst.addFirst("Toby");
+        }
         assertEquals(false, alst.isEmpty());
+    }
+
+    /** Test if the remove funciton works well*/
+    @Test
+    public void removeTest() {
+        ArrayDeque<Integer> alst = new ArrayDeque();
+        assertEquals(true, alst.isEmpty());
+        for (int i = 0; i < 10; i++) {
+            alst.addFirst(i);
+        }
+        for (int i = 0; i < 10; i++) {
+            assertEquals((Integer) i, alst.removeLast());
+        }
+    }
+
+    @Test
+    /**
+     * Add a few this to the list, checking get() is correct.
+     */
+    public void getTest() {
+        ArrayDeque<String> lld1 = new ArrayDeque<String>();
+        lld1.addFirst("Toby");
+        lld1.addFirst("love");
+        String actual = lld1.get(1);
+        String expected = "Toby";
+        assertEquals(expected, actual);
+        lld1.addFirst("I");
+        String actual2 = lld1.get(1);
+        String expected2 = "love";
+        assertEquals(expected, actual);
     }
 }
