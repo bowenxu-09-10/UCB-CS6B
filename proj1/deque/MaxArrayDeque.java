e230cc04f81211b4bb3022b5d9a43c8038bf40d3
Bowen <bowenxu.09.10@gmail.com>
2025-11-14 19:52:14 +0800

    Basically finish max() and constructure

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,15 +1,27 @@
 package deque;
 
+import java.util.Comparator;
+
 public class MaxArrayDeque<T> extends ArrayDeque<T>{
-    public MaxArrayDeque() {
+    private Comparator<T> comparator;
+    /**
+     * Creates a MaxArrayDeque with the given Comparator
+     */
+    public MaxArrayDeque(Comparator<T> c) {
         super();
+        comparator = c;
     }
 
-    public static void main(String[] args) {
-        MaxArrayDeque<String> mad = new MaxArrayDeque<>();
-        mad.addLast("I");
-        mad.addLast("love");
-        mad.addLast("Toby");
-        mad.printDeque();
+    public T max(Comparator<T> c) {
+        if (c == null) {
+            c = comparator;
+        }
+        T max = get(0);
+        for (int i = 0; i < this.size(); i++) {
+            if (c.compare(get(i), max) > 0) {
+                max = get(i);
+            }
+        }
+        return max;
     }
 }
