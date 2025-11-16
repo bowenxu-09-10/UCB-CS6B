b4305f56c02beeb8a85bc4b6678ffbd3903a00f0
Bowen <bowenxu.09.10@gmail.com>
2025-11-16 14:26:41 +0800

    Create meandata folder .capers

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -4,7 +4,7 @@ import java.io.File;
 import static capers.Utils.*;
 
 /** A repository for Capers 
- * @author TODO
+ * @author Bowen
  * The structure of a Capers Repository is as follows:
  *
  * .capers/ -- top level folder for all persistent data in your lab12 folder
@@ -18,8 +18,7 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = Utils.join("CWD", ".capers");
 
     /**
      * Does required filesystem operations to allow for persistence.
