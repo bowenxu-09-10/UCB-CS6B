632f37aeb22c294cc9f1060b85ecb6fc72326c3b
Bowen <bowenxu.09.10@gmail.com>
2025-11-18 20:56:52 +0800

    Create getContent, saveBlob function

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -1,13 +1,37 @@
 package gitlet;
 
 import java.io.File;
+import java.io.IOException;
+
 import static gitlet.Utils.*;
 
 public class Blob {
     /** The blob folder contains blob object. */
     public static final File BLOB_FOLDER = join(Repository.GITLET_DIR, "blob");
 
-    Blob() {
+    /** Save contents. */
+    private final byte[] contents;
 
+    /** Constructor. */
+    Blob(byte[] contents) {
+        this.contents = contents;
+    }
+
+    /** Return contents in certain Blob. */
+    public byte[] getContents(String blobName) {
+        File requiredBlob = join(BLOB_FOLDER, blobName);
+        requiredBlob.mkdirs();
+        return readContents(requiredBlob);
+    }
+
+    /** Save contents in a file named by sha-1. */
+    public void saveBlob() {
+        File newBlob = join(BLOB_FOLDER, sha1(contents));
+        try {
+            newBlob.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        writeContents(newBlob, contents);
     }
 }
