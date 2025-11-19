9d4bdbcc68c451e8aee61fd4eca964e943e2770b
Bowen <bowenxu.09.10@gmail.com>
2025-11-19 17:01:52 +0800

    Make saveFile return pid, fix bugs in check creat new check
    - checkSameCommit: check added file is the same as commited.
    - checkSameStage: check added file is the same as in stage area.

--- proj2/gitlet/Stage.java
+++ proj2/gitlet/Stage.java
@@ -15,9 +15,8 @@ public class Stage {
     /** The index file acts as staging area. */
     public static final File INDEX = join(Repository.GITLET_DIR, "index");
 
-    /** Save file as blob file, and return whether the file already exist.
-     *  True for exist, false for non-exist. */
-    private static boolean saveFile(String fileName) {
+    /** Save file as blob file, and return its pid. */
+    private static String saveFile(String fileName) {
         // Read contents in file and make a blob.
         Blob blob = new Blob(fileName);
         return blob.saveBlob();
@@ -25,20 +24,33 @@ public class Stage {
 
     /** Check if current file added is the same as commited.
      *  True for is the same as committed. */
-    private static boolean checkSame(String fileName) {
-        String pid = sha1(fileName);
+    private static boolean checkSameCommit(String fileName) {
         Commit head = Commit.getHeadCommit();
         HashMap fileTracted = head.fileNameToBLOB;
-        if (fileTracted.containsValue(pid)) {
+        if (fileTracted.containsValue(saveFile(fileName))) {
             return true;
         }
         return false;
     }
 
-    /** Add file to staging area. */
-    private static void addStage(String fileName) {
-        if (checkSame(fileName)) {
-            return;
+    /** Check if current file added is the same as staged.
+     *  True for is the same as committed. */
+    private static boolean checkSameStage(String fileName) {
+        Commit head = Commit.getHeadCommit();
+        HashMap fileTracted = head.fileNameToBLOB;
+        if (fileTracted.containsValue(saveFile(fileName))) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Add file to staging area. If added one is the same as commited, but in staged area,
+     *  then remove it from stagedAddition. */
+    public static void addStage(String fileName) {
+        if (checkSameCommit(fileName)) {
+            if (stagedAddition.containsValue(saveFile(fileName))) {
+                stagedAddition.remove(fileName);
+            }
         }
         stagedAddition.put(fileName, sha1(fileName));
     }
