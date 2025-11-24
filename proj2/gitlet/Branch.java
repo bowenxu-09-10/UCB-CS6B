07c7f661743c9813070cd015fe0d345e9a4b439c
Bowen <bowenxu.09.10@gmail.com>
2025-11-24 19:25:44 +0800

    Create writeOtherBranch method

--- proj2/gitlet/Branch.java
+++ proj2/gitlet/Branch.java
@@ -12,12 +12,12 @@ public class Branch {
     public static final File BRANCH_DIR = join(Repository.GITLET_DIR, "branches");
 
     /** Read Head to get the current branch name. */
-    private static String readHead() {
+    public static String readHead() {
         return readContentsAsString(HEAD);
     }
 
     /** Write current branch name into the HEAD. */
-    private static void writeHead(String branchName) {
+    public static void writeHead(String branchName) {
         writeContents(HEAD, branchName);
     }
 
@@ -37,16 +37,21 @@ public class Branch {
         }
         try {
             branch.createNewFile();
-            writeHead(branchName);
-            Branch.writeBranch(Commit.getHeadCommit().getPid());
+            Branch.writeOtherBranch(Commit.getHeadCommit().getPid(), branchName);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    /** Receive commitPID and write it into the branch file. */
+    /** Receive commitPID and write it into the current branch file. */
     public static void writeBranch(String commitPID) {
         File branch = join(BRANCH_DIR, readHead());
+        writeContents(branch, commitPID);
+    }
+
+    /** Receive commitPID and write it into other branch file. */
+    public static void writeOtherBranch(String commitPID, String branchName) {
+        File branch = join(BRANCH_DIR, branchName);
         writeContents(branch, commitPID);
     }
 
